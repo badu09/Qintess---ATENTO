76759da8e77acd6ee223fc0a7379677c33d097e0
badu09 <badu_09@hotmail.com>
2020-12-13 22:28:19 -0300

    alteração no css

--- Qintess - Atento - Alelo/Qintess - Atento - Alelo/force-app/main/default/aura/TabuladorAlelo/TabuladorAleloController.js
+++ Qintess - Atento - Alelo/Qintess - Atento - Alelo/force-app/main/default/aura/TabuladorAlelo/TabuladorAleloController.js
@@ -34,7 +34,7 @@
             {label: 'Contrato', fieldName: 'Numero_do_Contrato__c', type: 'text'},//Nome_do_Contrato__c
             {label: 'Banco', fieldName: 'Banco__c', type: 'text'},//Id_da_Conta__c
             {label: 'Produto', fieldName: 'Name', type: 'text'},//Name
-            {label: 'Fat Médio', fieldName: 'Id', type: 'currency', typeAttributes: { currencyCode: 'BR', maximumSignificantDigits: 5}},
+            {label: 'Fat Médio', fieldName: 'Faturamento__c', type: 'text'},
         ]);
 
     },
@@ -48,7 +48,139 @@
         helper.getAccountData(component, event, accountId);
         helper.getProdutoAlelo(component, accountId);
     },
-    consolidarId: function(component, event, helper){
+    consolidarId: function(cmp, event, helper){
+        console.log('Aqui Aqui');
+        var theMap = cmp.get("v.theMap");
             
+        var idList = [cmp.find("grupoRel").get("v.value")];
+                      
+        if(idList != ''){
+            theMap["cnpj"] = cmp.find("textAccountCNPJ").get("v.value");
+            theMap["razaoSocial"] = cmp.find("textAccountRazaoSocial").get("v.value");
+            theMap["folhaDePagto"] = cmp.find("folhaPgto").get("v.value");
+            theMap["interlocutor"] = cmp.find("interlocutor").get("v.value");
+            if(cmp.find("dataUltimoContato").get("v.value") != ''){
+                theMap["dataDoUltimoContato"] = cmp.find("dataUltimoContato").get("v.value");
+            } else {
+                theMap["dataDoUltimoContato"] = null;
+            }
+            if(cmp.find("dataAgendProxContato").get("v.value") != ''){
+                theMap["dataAgendamentoProxContato"] = cmp.find("dataAgendProxContato").get("v.value");
+            } else {
+                theMap["dataAgendamentoProxContato"] = null;
+            }
+            theMap["risco"] = cmp.find("risco").get("v.value");
+            if(cmp.find("mesRetencao").get("v.value") != ''){
+                theMap["mesDeRetencao"] = cmp.find("mesRetencao").get("v.value");
+            } else {
+                theMap["mesDeRetencao"] = null;
+            }
+            theMap["prazoDeBlindagem"] = cmp.find("prazodeBlindagem").get("v.value");
+            theMap["statusDoGrupo"] = cmp.find("statusdoGrupo").get("v.value");
+            theMap["fatorDeRisco"] = cmp.find("fator_deRisco").get("v.value");
+            theMap["statusDoRisco"] = cmp.find("statusdeRisco").get("v.value");
+            theMap["acaoPosPerda"] = cmp.find("acaoPosPerda").get("v.value");
+            if(cmp.find("mesPosPerda").get("v.value") != ''){
+                theMap["mesDePerda"] = cmp.find("mesPosPerda").get("v.value");
+            } else {
+                theMap["mesDePerda"] = null;
+            }
+            theMap["telefone"] = cmp.find("textTelefone").get("v.value");
+            theMap["email"] = cmp.find("textEmail").get("v.value");
+            theMap["fornAlimentacao"] = cmp.find("fornAtualAlimentacao").get("v.value");
+            theMap["statusAlimentacao"] = cmp.find("statusFornAtualAlimentacao").get("v.value");
+            theMap["fornRefeicao"] = cmp.find("fornAtualRefeicao").get("v.value");
+            theMap["statusRefeicao"] = cmp.find("statusFornAtualRefeicao").get("v.value");
+            theMap["fornTransporte"] = cmp.find("fornAtualTransporte").get("v.value");
+            theMap["statusTransporte"] = cmp.find("statusFornAtualTransporte").get("v.value");
+            theMap["fornCombustivel"] = cmp.find("fornAtualCombustivel").get("v.value");
+            theMap["statusCombustivel"] = cmp.find("statusFornAtualCombustivel").get("v.value");
+            theMap["fornNatal"] = cmp.find("fornAtualNatal").get("v.value");
+            theMap["statusNatal"] = cmp.find("statusFornAtualNatal").get("v.value");
+            theMap["fornPremiacao"] = cmp.find("fornAtualPremiacao").get("v.value");
+            theMap["statusPremiacao"] = cmp.find("statusFornAtualPremiacao").get("v.value");
+            theMap["fornDespesas"] = cmp.find("fornAtualDespesas").get("v.value");
+            theMap["statusDespesas"] = cmp.find("statusFornAtualDespesas").get("v.value");
+            theMap["fornPagamentos"] = cmp.find("fornAtualPagamentos").get("v.value");
+            theMap["statusPagamentos"] = cmp.find("statusFornAtualPagamentos").get("v.value");
+            
+            
+            //console.log('never', params);
+            console.log('idList', idList);
+            console.log('map', theMap);
+            helper.consolidar(cmp, event, idList, theMap, true);    
+        } else {
+            helper.showToastMessage(cmp, event, 'Erro', 'Grupo_Rel não pode ficar vazio', 'Error');
+        }
+        
+        
+    }, 
+    consolidarCNPJ: function(cmp, event, helper){
+        
+        var idList = cmp.get("v.idLists");
+        
+        if(idList != ''){
+            var theMap = cmp.get("v.theMap");
+            
+            theMap["cnpj"] = cmp.find("textAccountCNPJ").get("v.value");
+            theMap["razaoSocial"] = cmp.find("textAccountRazaoSocial").get("v.value");
+            theMap["folhaDePagto"] = cmp.find("folhaPgto").get("v.value");
+            theMap["interlocutor"] = cmp.find("interlocutor").get("v.value");
+            if(cmp.find("dataUltimoContato").get("v.value") != ''){
+                theMap["dataDoUltimoContato"] = cmp.find("dataUltimoContato").get("v.value");
+            } else {
+                theMap["dataDoUltimoContato"] = null;
+            }
+            if(cmp.find("dataAgendProxContato").get("v.value") != ''){
+                theMap["dataAgendamentoProxContato"] = cmp.find("dataAgendProxContato").get("v.value");
+            } else {
+                theMap["dataAgendamentoProxContato"] = null;
+            }
+            theMap["risco"] = cmp.find("risco").get("v.value");
+            if(cmp.find("mesRetencao").get("v.value") != ''){
+                theMap["mesDeRetencao"] = cmp.find("mesRetencao").get("v.value");
+            } else {
+                theMap["mesDeRetencao"] = null;
+            }
+            theMap["prazoDeBlindagem"] = cmp.find("prazodeBlindagem").get("v.value");
+            theMap["statusDoGrupo"] = cmp.find("statusdoGrupo").get("v.value");
+            theMap["fatorDeRisco"] = cmp.find("fator_deRisco").get("v.value");
+            theMap["statusDoRisco"] = cmp.find("statusdeRisco").get("v.value");
+            theMap["acaoPosPerda"] = cmp.find("acaoPosPerda").get("v.value");
+            if(cmp.find("mesPosPerda").get("v.value") != ''){
+                theMap["mesDePerda"] = cmp.find("mesPosPerda").get("v.value");
+            } else {
+                theMap["mesDePerda"] = null;
+            }
+            theMap["telefone"] = cmp.find("textTelefone").get("v.value");
+            theMap["email"] = cmp.find("textEmail").get("v.value");
+            theMap["fornAlimentacao"] = cmp.find("fornAtualAlimentacao").get("v.value");
+            theMap["statusAlimentacao"] = cmp.find("statusFornAtualAlimentacao").get("v.value");
+            theMap["fornRefeicao"] = cmp.find("fornAtualRefeicao").get("v.value");
+            theMap["statusRefeicao"] = cmp.find("statusFornAtualRefeicao").get("v.value");
+            theMap["fornTransporte"] = cmp.find("fornAtualTransporte").get("v.value");
+            theMap["statusTransporte"] = cmp.find("statusFornAtualTransporte").get("v.value");
+            theMap["fornCombustivel"] = cmp.find("fornAtualCombustivel").get("v.value");
+            theMap["statusCombustivel"] = cmp.find("statusFornAtualCombustivel").get("v.value");
+            theMap["fornNatal"] = cmp.find("fornAtualNatal").get("v.value");
+            theMap["statusNatal"] = cmp.find("statusFornAtualNatal").get("v.value");
+            theMap["fornPremiacao"] = cmp.find("fornAtualPremiacao").get("v.value");
+            theMap["statusPremiacao"] = cmp.find("statusFornAtualPremiacao").get("v.value");
+            theMap["fornDespesas"] = cmp.find("fornAtualDespesas").get("v.value");
+            theMap["statusDespesas"] = cmp.find("statusFornAtualDespesas").get("v.value");
+            theMap["fornPagamentos"] = cmp.find("fornAtualPagamentos").get("v.value");
+            theMap["statusPagamentos"] = cmp.find("statusFornAtualPagamentos").get("v.value");
+            
+            //console.log('never', params);
+            console.log('listId', idList);
+            console.log('map', theMap);
+            helper.consolidar(cmp, event, idList, theMap, false);
+        } else {
+            helper.showToastMessage(cmp, event, 'Erro', 'Grupo_Rel não pode ficar vazio, digite um Id para procurar um grupo valido', 'Error');
+        }
+            
+    },
+    reset: function(cmp, event, helper){
+        helper.resetFields(cmp, event);
     }
 })
--- Qintess - Atento - Alelo/Qintess - Atento - Alelo/force-app/main/default/aura/TabuladorAlelo/TabuladorAleloHelper.js
+++ Qintess - Atento - Alelo/Qintess - Atento - Alelo/force-app/main/default/aura/TabuladorAlelo/TabuladorAleloHelper.js
@@ -10,6 +10,7 @@
         	grupoRelId: groupId
         });
         var accountList = [];
+        var idList = [];
         action.setCallback(this, function (response) {
             var state = response.getState();
             var data = response.getReturnValue();
@@ -18,11 +19,13 @@
                 if(data.length > 0){
                     for (var dt in data){
                         accountList.push({label: data[dt].Name, value: data[dt].Id});
+                        idList.push(data[dt].Id);
                     }
                 } else {
 					accountList.push({label: "Lista de Contas vazia", value: ""});                    
                 }
                 cmp.set("v.accountList", accountList);
+                cmp.set("v.idLists", idList);
             } else {
                 var errors = data.getError();
                 if (errors[0] && errors[0].message) {
@@ -74,6 +77,36 @@
                 cmp.find("textAccountRazaoSocial").set("v.value", data.Razao_Social__c);
                 cmp.find("textTelefone").set("v.value", data.TelefoneAtual__c);
                 cmp.find("textEmail").set("v.value", data.Email__c);
+                
+                cmp.find("textAccountRazaoSocial").set("v.value", data.Razao_Social__c);
+                cmp.find("folhaPgto").set("v.value", data.Folha_de_Pagto__c);
+                cmp.find("interlocutor").set("v.value", data.Interlocutor__c);
+                cmp.find("dataUltimoContato").set("v.value", data.Data_do_ultimo_Contato__c);
+                cmp.find("dataAgendProxContato").set("v.value", data.Data_Agendamento_prox_Contato__c);
+                cmp.find("risco").set("v.value", data.Risco__c);
+                cmp.find("mesRetencao").set("v.value", data.M_s_de_Reten_o__c);
+                cmp.find("prazodeBlindagem").set("v.value", data.Prazo_de_Blindagem__c);
+                cmp.find("statusdoGrupo").set("v.value", data.Status_do_Grupo__c);
+                cmp.find("fator_deRisco").set("v.value", data.Fator_de_Risco__c);
+                cmp.find("statusdeRisco").set("v.value", data.Status_do_Risco__c);
+                cmp.find("acaoPosPerda").set("v.value", data.A_o_P_s_Perda__c);
+                cmp.find("mesPosPerda").set("v.value", data.Mes_de_Perda__c);
+                cmp.find("fornAtualAlimentacao").set("v.value", data.FORNECEDOR_ATUAL_ALIMENTACAO__c);
+                cmp.find("fornAtualRefeicao").set("v.value", data.FORNECEDOR_ATUAL_REFEICAO__c);
+                cmp.find("fornAtualTransporte").set("v.value", data.FORNECEDOR_ATUAL_TRANSPORTE__c);
+                cmp.find("fornAtualCombustivel").set("v.value", data.FORNECEDOR_ATUAL_COMBUSTIVEL__c);
+                cmp.find("statusFornAtualAlimentacao").set("v.value", data.Status_Negociacao_Alimentacao__c);
+                cmp.find("statusFornAtualRefeicao").set("v.value", data.Status_Negociacao_Refeicao__c);
+                cmp.find("statusFornAtualTransporte").set("v.value", data.Status_Negociacao_Transporte__c);
+                cmp.find("statusFornAtualCombustivel").set("v.value", data.Status_Negociacao_Combustivel__c);
+                cmp.find("fornAtualNatal").set("v.value", data.FORNECEDOR_ATUAL_NATAL__c);
+                cmp.find("fornAtualPremiacao").set("v.value", data.FORNECEDOR_ATUAL_PR_PAGOS_Premia_o_in__c);
+                cmp.find("fornAtualDespesas").set("v.value", data.PR_PAGOS_Despesas__c);
+                cmp.find("fornAtualPagamentos").set("v.value", data.Fornecedor_Atual_de_Pagamentos__c);
+                cmp.find("statusFornAtualNatal").set("v.value", data.Status_Negociacao_Natal__c);
+                cmp.find("statusFornAtualPremiacao").set("v.value", data.Status_Negocia_o_PR_PAGOS_Premia_o__c);
+                cmp.find("statusFornAtualDespesas").set("v.value", data.Status_Negocia_o_Despesas__c);
+                cmp.find("statusFornAtualPagamentos").set("v.value", data.Status_Negocia_o_Pagamentos__c);
                 if(data.Grupo_de_Relacionamento__c != null){
                     this.getGrupoRelacionamento(cmp, data.Grupo_de_Relacionamento__c);
                 }
@@ -119,32 +152,85 @@
             } else {
                 var errors = data.getError();
                 if (errors[0] && errors[0].message) {
+                    this.showToastMessage(cmp, event, 'Erro', errors[0].message, 'Error');
                 	console.log("Error message: " + errors[0].message);
                 }
             }
         });
         $A.enqueueAction(action);
     },
-    consolida: function (cmp, event, idList, mapParams){
+    consolidar: function (cmp, event, idList, mapParams, isCnpj){
+        cmp.set("v.loading", true);
         var action = cmp.get("c.consolidar");
         action.setParams({
         	ids: idList,
-            params: mapParams
+            params: mapParams,
+            isCnpj: isCnpj
         });
         action.setCallback(this, function (response) {
             var state = response.getState();
-            var data = response.getReturnValue();
             if(state === "SUCCESS"){
-                //cmp.set('v.dataTable', data);
+                this.showToastMessage(cmp, event, 'Sucesso', 'Informações salvas com sucesso', 'Success');
                 console.log("success");
             } else {
-                var errors = data.getError();
+                var errors = response.getError();
                 if (errors[0] && errors[0].message) {
+                    this.showToastMessage(cmp, event, 'Erro', errors[0].message, 'Error');
                 	console.log("Error message: " + errors[0].message);
                 }
             }
         });
         $A.enqueueAction(action);
+        cmp.set("v.loading", false);
+    },
+    showToastMessage: function(cmp, event, title, message, type){
+    	var toast = $A.get("e.force:showToast");
+        if (toast){
+            //fire the toast event in Salesforce app and Lightning Experience
+            toast.setParams({
+                "title": title,
+                "message": message,
+                "type": type
+            });
+            toast.fire();
+        }
+    },
+    resetFields: function (cmp, event){
+        cmp.find("groupIdText").set("v.value", "");
+        cmp.find("textAccountCNPJ").set("v.value", "");
+        cmp.find("textAccountRazaoSocial").set("v.value", "");
+        cmp.find("textTelefone").set("v.value", "");
+        cmp.find("textEmail").set("v.value", "");
+        
+        cmp.find("textAccountRazaoSocial").set("v.value", "");
+        cmp.find("folhaPgto").set("v.value", "");
+        cmp.find("interlocutor").set("v.value", "");
+        cmp.find("dataUltimoContato").set("v.value", "");
+        cmp.find("dataAgendProxContato").set("v.value", "");
+        cmp.find("risco").set("v.value", "");
+        cmp.find("mesRetencao").set("v.value", "");
+        cmp.find("prazodeBlindagem").set("v.value", "");
+        cmp.find("statusdoGrupo").set("v.value", "");
+        cmp.find("fator_deRisco").set("v.value", "");
+        cmp.find("statusdeRisco").set("v.value", "");
+        cmp.find("acaoPosPerda").set("v.value", "");
+        cmp.find("mesPosPerda").set("v.value", "");
+        cmp.find("fornAtualAlimentacao").set("v.value", "");
+        cmp.find("fornAtualRefeicao").set("v.value", "");
+        cmp.find("fornAtualTransporte").set("v.value", "");
+        cmp.find("fornAtualCombustivel").set("v.value", "");
+        cmp.find("statusFornAtualAlimentacao").set("v.value", "");
+        cmp.find("statusFornAtualRefeicao").set("v.value", "");
+        cmp.find("statusFornAtualTransporte").set("v.value", "");
+        cmp.find("statusFornAtualCombustivel").set("v.value", "");
+        cmp.find("fornAtualNatal").set("v.value", "");
+        cmp.find("fornAtualPremiacao").set("v.value", "");
+        cmp.find("fornAtualDespesas").set("v.value", "");
+        cmp.find("fornAtualPagamentos").set("v.value", "");
+        cmp.find("statusFornAtualNatal").set("v.value", "");
+        cmp.find("statusFornAtualPremiacao").set("v.value", "");
+        cmp.find("statusFornAtualDespesas").set("v.value", "");
+        cmp.find("statusFornAtualPagamentos").set("v.value", "");
     }
     
 })
